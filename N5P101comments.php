dab7101b6a8159996b448188a2519ac00545404a
MartinezTech <alexmartinez.mm98@gmail.com>
2021-01-06 22:45:36 +0100

    add task comments

---
+++ N5P101comments.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments PAC01 Administrative Pages</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Comments for PAC01 - Administrative pages</h1>
+        
+        <h3>List of learn concepts</h3>
+        <ol>
+            <li>How to manage mysql database from a php interface</li>
+            <li>How to set up the environment to use admin interface</li>
+            <li>How to add a movie into database from the admin interface</li>
+            <li>How to edit rows from our database from the admin interface using php</li>
+            <li>How to delete movies from the admin interface</li>
+            <li>How to create forms with the data provided from our database</li>
+            <li>Use of switch statement in php</li>
+            <li>Populate the select options with mysql query results</li>
+            <li>Use of if/else statement</li>
+            <li>How to use update with php and mysql in order to edit rows</li>
+        </ol>
+        <h3>Document and teacher explanation</h3>
+        <p>I think it's ok.
+        </p>
+        <h3>Mark</h3>
+        <p>
+            I always try to do my best.
+        </p>
+        <h3>Improvements</h3>
+        <p>I don't see any improvement in this task</p>
+    </div>
+    
+</body>
+</html>
